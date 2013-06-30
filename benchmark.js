d43992b55dd0d9f25724e063884bd0583073dae0
Fredrik Olofsson <a@a.nu>
2013-06-30 19:14:42 +0200

    Added benchmark script.

---
+++ benchmark.js
@@ -0,0 +1,84 @@
+﻿var ByteVector = require('./index');
+
+process.stdout.write('Benchamrking ByteVector, please wait...\n\n');
+
+
+var vector = ByteVector();
+
+
+var tests = [
+  {
+    name: "push_back",
+    prepare: function(testcount) {
+      return new ByteVector();
+    },
+    handler: function (vector) {
+      vector.push_back(2);
+    }
+  },
+  {
+    name: "push_back with reserve",
+    prepare: function (testcount) {
+      return new ByteVector(testcount);
+    },
+    handler: function (vector) {
+      vector.push_back(2);
+    }
+  },
+  {
+    name: "pop_back",
+    prepare: function (testcount) {
+      var vector = new ByteVector(testcount);
+      while (0 < --testcount) {
+        vector.push_back(3);
+      }
+      return vector;
+    },
+    handler: function (vector) {
+      vector.pop_back();
+    }
+  },
+  {
+    name: "pop_front",
+    prepare: function (testcount) {
+      var vector = new ByteVector(testcount);
+      while (0 < --testcount) {
+        vector.push_back(3);
+      }
+      return vector;
+    },
+    handler: function (vector) {
+      vector.pop_front();
+    }
+  }
+];
+
+
+var NumberOfTests = 1024*1024;
+tests.forEach(function (test) {
+  process.stdout.write('\nRunning test: "' + test.name + '"...');
+
+  var avgOperationsPerMS = [];
+  for (var i = 0, len = 100; i < len; ++i) {
+    var counter = NumberOfTests,
+        vector = test.prepare(NumberOfTests),
+        handler = test.handler,
+        startTime = (new Date()).getTime();
+
+    while (0 < --counter) {
+      handler(vector);
+    }
+    var endTime = (new Date()).getTime(),
+        diff = endTime - startTime,
+        operationsPerMS = NumberOfTests / diff;
+
+    avgOperationsPerMS.push(operationsPerMS);
+  }
+
+  var total = 0;
+  avgOperationsPerMS.forEach(function (opms) {
+    total += opms;
+  });
+  var avg = total/avgOperationsPerMS.length;
+  process.stdout.write('\n\t' + avg + '/ms');
+});
